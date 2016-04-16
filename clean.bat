cc419f3fea35d64482a7761060cc2dfa14c716b2
kevin <janyboo@gmail.com>
2016-04-17 03:12:02 +0800

    Add clean.bat to clean project

---
+++ clean.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+MSBUILD TopUI.sln /t:Clean /p:Configuration=Debug
+MSBUILD TopUI.sln /t:Clean /p:Configuration=Release
+MSBUILD TopUI.sln /t:Clean /p:Configuration=UnicodeDebug
+MSBUILD TopUI.sln /t:Clean /p:Configuration=UnicodeRelease
+
+RD /S /Q  demo\Launcher\Debug
+RD /S /Q  demo\Launcher\Release
+RD /S /Q  demo\Launcher\Debug_u
+RD /S /Q  demo\Launcher\Release_u
+    
+RD /S /Q  demo\TestApp1\Debug
+RD /S /Q  demo\TestApp1\Release
+RD /S /Q  demo\TestApp1\Debug_u
+RD /S /Q  demo\TestApp1\Release_u
+
+exit /b %ERRORLEVEL%
+endlocal
+pause
