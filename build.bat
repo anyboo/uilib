d0fc008c23cf9f6836c16982d58d3946bdc09ff9
kevin <janyboo@gmail.com>
2016-04-20 15:54:31 +0800

    Update build script Add Usage help Add Argument for build specify project

--- build.bat
+++ build.bat
@@ -1,10 +1,35 @@
 @echo off
 setlocal
-call "%VS120COMNTOOLS%vsvars32.bat
-MSBUILD TopUI.sln /t:Rebuild /p:Configuration=Debug
-MSBUILD TopUI.sln /t:Rebuild /p:Configuration=Release
-MSBUILD TopUI.sln /t:Rebuild /p:Configuration=UnicodeDebug
-MSBUILD TopUI.sln /t:Rebuild /p:Configuration=UnicodeRelease
-exit /b %ERRORLEVEL%
-endlocal
+IF /I "%1"=="" goto usage
+@call "%VS120COMNTOOLS%vsvars32.bat
+
+SET PROJECT=%1
+SET CONFIGURATION=%2
+echo %PROJECT%
+
+IF /I "%PROJECT%"=="all" (
+	SET PROJECT=Rebuild
+) ELSE (
+    SET PROJECT=%PROJECT%:rebuild
+)
+
+goto msbuild
 pause
+
+:msbuild
+MSBUILD TopUI.sln /t:%PROJECT% /p:Configuration=Debug
+MSBUILD TopUI.sln /t:%PROJECT% /p:Configuration=Release
+MSBUILD TopUI.sln /t:%PROJECT% /p:Configuration=UnicodeDebug
+MSBUILD TopUI.sln /t:%PROJECT% /p:Configuration=UnicodeRelease
+endlocal
+exit /b %ERRORLEVEL%
+
+
+:usage
+echo Usage:
+echo ------
+echo build [PROJECT]
+echo PROJECT: ".vcxproj|all"
+echo.
+echo Default is build all.
+endlocal
