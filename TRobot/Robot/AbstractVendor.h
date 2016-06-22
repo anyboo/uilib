5cc33c57748809bd0e25e64c6c82fae1e6afccbc
kevin <janyboo@gmail.com>
2016-06-22 10:38:36 +0800

    修改Download接口，时刻转换成时间区域

--- TRobot/Robot/AbstractVendor.h
+++ TRobot/Robot/AbstractVendor.h
@@ -12,8 +12,8 @@ public:
 	virtual void Login(const std::string& user, const std::string& password) = 0;
 	virtual void Logout() = 0;
 	virtual void SearchAll() = 0;
-	virtual void SearchByTime(const std::time_t& time) = 0;
-	virtual void DownloadByTime(const std::time_t& time) = 0;
+	virtual void SearchByTime(const std::time_t& start, const std::time_t& end) = 0;
+	virtual void DownloadByTime(const std::time_t& start, const std::time_t& end) = 0;
 	virtual void DownloadByName(const std::string& filename) = 0;
 	virtual void PlayVideo(const std::string& filename) = 0;
 };
