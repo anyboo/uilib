724e20f306c241d09801f8cbc2e2699b3fad02e1
mk-z <zyf079@gmail.com>
2016-06-24 16:04:37 +0800

    dababase code

--- transation/QMSqlite.h
+++ transation/QMSqlite.h
@@ -1,4 +1,18 @@
-#pragma once
+#ifndef QM_SQLITE_H
+#define QM_SQLITE_H
+
+#include "Poco/Data/Session.h"
+#include "Poco/Data/SessionPool.h"
+#include "Poco/Data/SQLite/Connector.h"
+#include <iostream>
+#include <vector>
+
+typedef Poco::Tuple<std::string, int, __time64_t, __time64_t, LONGLONG, std::string> SearchRecord;
+
+
+using namespace Poco::Data;
+using namespace std;
+
 class QMSqlite
 {
 public:
@@ -8,13 +22,49 @@ private:
 	QMSqlite();
 	~QMSqlite();
 	static const QMSqlite* m_instance;
+
+	class Garbo
+
+	{
+
+	public:
+
+		~Garbo()
+
+		{
+
+			if (QMSqlite::m_instance)
+
+			{
+
+				//cout << "Garbo dtor" << endl;
+
+				delete QMSqlite::m_instance;
+
+			}
+
+		}
+
+	};
+
+	static Garbo garbo;
 public:
-	int Initialize();
-	int creatDatabase();
-	int connectDb();
-	int createSearchTable();
-	int GetSearchData();
-	int writeSearchData();
-	int cleanSearchData();
+	
+	bool createSearchTable();
+	bool GetSearchData(std::vector<SearchRecord>&Record);
+	bool writeSearchData(SearchRecord searchrecode);
+	bool writeSearchDataByVector(std::vector<SearchRecord>&Record);
+	bool cleanSearchData();
+	bool dropSearchTable();
+private:
+	bool Initialize();
+	bool creatSessionPool();
+	void closeConnect(Session sess);
+	bool checkConnect(Session sess);
+	Session connectDb();
+	SessionPool *m_pool;
+	
 };
 
+
+#endif 
